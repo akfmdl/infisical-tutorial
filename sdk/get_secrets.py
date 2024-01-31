ce8f737b83d1d054861736eb43feadddb399de1f
gorani <akfmdl@naver.com>
2024-01-31 14:43:48 +0900

    update comment

--- sdk/get_secrets.py
+++ sdk/get_secrets.py
@@ -8,6 +8,9 @@ client = InfisicalClient(site_url=INFISICAL_API_URL, token=INFISICAL_TOKEN)
 secret = client.get_secret(secret_name="DATABASE_URL", environment="dev", type="shared")
 secrets = client.get_all_secrets()
 
-print(f"DATABASE_URL: {secret.secret_value}")
+print("\nSingle Secret:")
+print(f"DATABASE_URL: {secret.secret_value}\n")
+
+print("All Secrets:")
 for secret in secrets:
     print(f"{secret.secret_name}: {secret.secret_value}")
